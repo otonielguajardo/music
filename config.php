16794a13630395e33fa5d71f9fdbd06ac9d8103c
Otoniel Guajardo <otonielguajardo@gmail.com>
2024-07-26 22:57:48 -0600

    migrate project to jigsaw framwork

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'production' => false,
+    'baseUrl' => '',
+    'title' => 'Otoniel Guajardo',
+    'description' => '',
+    'collections' => [
+        'albums'
+    ],
+];
---
+++ webpack.mix.js
@@ -0,0 +1,13 @@
+const mix = require('laravel-mix');
+require('laravel-mix-jigsaw');
+
+mix.disableSuccessNotifications();
+mix.setPublicPath('source/assets/build');
+
+mix.jigsaw()
+    .js('source/_assets/js/main.js', 'js')
+    .css('source/_assets/css/main.css', 'css')
+    .options({
+        processCssUrls: false,
+    })
+    .version();
